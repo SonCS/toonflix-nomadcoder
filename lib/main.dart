5ed6798341a993c1e20d209bf06172e7859d6e26
SonCS <hp3943@hanmail.net>
2024-02-26 14:59:16 +0900

    3.0 Header

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
+class Player {
+  String? name;
+
+  Player();
+}
+
 void main() {
+  var chungs = Player();
   runApp(App());
 }
 
@@ -11,21 +18,42 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          centerTitle: false,
-          title: const Text(
-            'Hello, Flutter!',
-            style: TextStyle(
-              color: Colors.white)
-              ),
-          backgroundColor: Colors.deepPurple,
-          foregroundColor: Colors.white,
-        ),
-        body: const Center(
-          child: Text('hello, World!'),
-        )
+        backgroundColor: Color(0xff181818),
+        body: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 18),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 80,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          'Hey, Chungs',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 28,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Text(
+                          'Welcom, back',
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(0.8),
+                            fontSize: 18,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                )
+              ],
+            )),
       ),
     );
   }
 }
-
