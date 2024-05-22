726fca1eb3871bbb0283db3f50d4f4554c5a9b4f
SonCS <hp3943@hanmail.net>
2024-05-22 14:56:54 +0900

    3.3 Button Section

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
 
-class Player {
-  String? name;
-
-  Player();
-}
-
 void main() {
-  var chungs = Player();
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
@@ -20,39 +13,77 @@ class App extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Color(0xff181818),
         body: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 18),
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 80,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Text(
-                          'Hey, Chungs',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 28,
-                            fontWeight: FontWeight.w700,
-                          ),
+          padding: EdgeInsets.symmetric(horizontal: 18),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      const Text(
+                        'Hey, Chungs',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w700,
                         ),
-                        Text(
-                          'Welcom, back',
-                          style: TextStyle(
-                            color: Colors.white.withOpacity(0.8),
-                            fontSize: 18,
-                          ),
-                        )
-                      ],
-                    )
-                  ],
-                )
-              ],
-            )),
+                      ),
+                      Text(
+                        'Welcom, back',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.8),
+                          fontSize: 18,
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 120,
+              ),
+              Text(
+                'Total Balance',
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+              ),
+              const Text(
+                "\$ 100000000000000",
+                style: TextStyle(
+                  fontSize: 42,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: [
+                  Container(
+                      // div와 같은 위젯
+                      decoration: BoxDecoration(color: Colors.amber,
+                      borderRadius: BorderRadius.circular(45)),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 20,
+                          horizontal: 50,
+                        ),
+                        child: Text('Transfer', style: TextStyle(fontSize: 20),),
+                      ))
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
