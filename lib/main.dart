6e40ad7fd9d10273e778c7aa70ea8c8036767d76
SonCS <hp3943@hanmail.net>
2024-05-22 17:33:55 +0900

    3.7 Icons adn Transforms - flutter sdk에서 많은 아이콘을 제공 - transforms을 통해 scale, translate 등을 사용

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class App extends StatelessWidget {
                 ],
               ),
               const SizedBox(
-                height: 120,
+                height: 20,
               ),
               Text(
                 'Total Balance',
@@ -108,13 +108,15 @@ class App extends StatelessWidget {
                 height: 20,
               ),
               Container(
+                clipBehavior: Clip.hardEdge,
                 decoration: BoxDecoration(
                   color: const Color(0xff1f2123),
                   borderRadius: BorderRadius.circular(20),
                 ),
-                child:  Padding(
+                child: Padding(
                   padding: const EdgeInsets.all(30),
                   child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
@@ -139,7 +141,9 @@ class App extends StatelessWidget {
                                   fontSize: 20,
                                 ),
                               ),
-                              const SizedBox(width: 5,),
+                              const SizedBox(
+                                width: 5,
+                              ),
                               Text(
                                 'EUR',
                                 style: TextStyle(
@@ -150,6 +154,17 @@ class App extends StatelessWidget {
                             ],
                           ),
                         ],
+                      ),
+                      Transform.scale(
+                        scale: 2.2,
+                        child: Transform.translate(
+                          offset: const Offset(-5, 13),
+                          child: const Icon(
+                            Icons.euro_rounded,
+                            color: Colors.white,
+                            size: 88,
+                          ),
+                        ),
                       ),
                     ],
                   ),
