39bb0a59de1c1c07093ecc1f091b02196ade0e15
SonCS <hp3943@hanmail.net>
2024-06-22 15:32:45 +0900

    6.1  Introduction - 사용할 api 설명 - 프로젝트 초기화

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:toonflix/srceens/home_screen.dart';
 
 void main() {
   runApp(const App());
 }
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
-
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  bool showTitle = true;
-
-  void toggleTitle() {
-    setState(() {
-      showTitle = !showTitle;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-            color: Color(0xFF232B55),
-          ),
-          titleLarge: TextStyle(
-            color: Colors.red,
-          ),
-        ),
-        colorScheme: ColorScheme.fromSwatch(
-          backgroundColor: const Color(0xFFE7626C),
-        ),
-        cardColor: const Color(0xFFF4EDDB)
-      ),
-      home: const HomeScreen(),
+      home: Container(),
     );
   }
-}
 
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  int count = 0;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-
-    print('initState!');
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    print('dispose');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print('build');
-    return Text(
-      'My Large Title',
-      style: TextStyle(
-          fontSize: 30, color: Theme.of(context).textTheme.titleLarge?.color),
-    );
-  }
 }
