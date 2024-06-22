b84e6dba9e20bb0b30621f3664ba4ec0c984a768
SonCS <hp3943@hanmail.net>
2024-06-22 16:15:19 +0900

    6.2 Data Fetching - pub.dev 설명 - http 통신 처리

---
+++ lib/services/api_service.dart
@@ -0,0 +1,20 @@
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
+
+  // endpoint
+  final String today = "today";
+
+
+  void getTodaysToons() async {
+    final url = Uri.parse('$baseUrl/$today');
+    
+    final response = await http.get(url);
+    if(response.statusCode == 200) {
+      print(response.body);
+      return;
+    } 
+    throw Error();
+  }
+}
