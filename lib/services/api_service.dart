bf009d69cfb65708f91692be21a1b54b7408b33d
SonCS <hp3943@hanmail.net>
2024-06-22 16:35:01 +0900

    6.3 fromJson - http 통신을 통해 전달받은 데이터(body)를 json으로 변환 - json을 클래스로 변환

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
@@ -6,15 +9,17 @@ class ApiService {
   // endpoint
   final String today = "today";
 
-
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonItems = [];
     final url = Uri.parse('$baseUrl/$today');
-    
     final response = await http.get(url);
-    if(response.statusCode == 200) {
-      print(response.body);
-      return;
-    } 
+    if (response.statusCode == 200) {
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        webtoonItems.add(WebtoonModel.fromJson(webtoon));
+      }
+      return webtoonItems;
+    }
     throw Error();
   }
 }
